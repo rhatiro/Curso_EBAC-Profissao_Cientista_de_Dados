30e5f5ac82e4cef3ecbf5f3d94e08d8929d30b13
Roberto Hatiro Nishiyama <104247925+rhatiro@users.noreply.github.com>
2023-11-30 00:24:46 -0300

    Update mod31_ex2_projeto_AgrupamentoHierarquico.py

--- Modulo_31_-_Streamlit_V/Exercicio_2/mod31_ex2_projeto_AgrupamentoHierarquico.py
+++ Modulo_31_-_Streamlit_V/Exercicio_2/mod31_ex2_projeto_AgrupamentoHierarquico.py
@@ -213,7 +213,7 @@ def main():
     with st.echo():
         ""
         # Ler o arquivo CSV 'online_shoppers_intention.csv' e armazenar os dados em um DataFrame chamado df
-        df = pd.read_csv('online_shoppers_intention.csv')
+        df = pd.read_csv('./online_shoppers_intention.csv')
 
         # Exibir o DataFrame df, mostrando os dados carregados do arquivo CSV
         st.dataframe(df)
